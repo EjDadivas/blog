1ef5cf05c06b231f47ff4cff4bf4f87c1c417c2b
ashcbrd <99721216+EjDadivas@users.noreply.github.com>
2023-07-14 20:17:17 +0800

    improved file structure

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,23 @@ class Post extends Model
 
     protected $guarded = [];
     protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters){
+
+        $query->when($filters['search'] ?? false, fn($query, $search)=>
+            $query
+            ->where('title', 'like', '%' .$search.'%')
+            ->orWhere('body', 'like', '%' .$search.'%'));
+        $query->when($filters['category'] ?? false, fn($query, $category)=>
+            $query
+            //     ->whereExists(fn($query)=>
+            //     $query->from('categories')
+            //         ->whereColumn('categories.id', 'posts.category_id')
+            //         ->where('categories.slug', $category)
+            ->whereHas('category', fn($query) =>
+            $query->where('slug', $category)));
+
+    }
     public function getRouteKeyName(): string
     {
         return 'slug';
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,19 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    $posts = Post::latest();
-    if(request('search')){
-        $posts->where('title', 'like', '%' .request('search').'%')
-        ->orWhere('title', 'like', '%' .request('search').'%');
-    }
-    return view('posts',[
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
-
-//category has many posts therefore $cateogry->posts
-Route::get('categories/{category:slug}', function(Category $category){
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
+Route::get('posts/{post}', [PostController::class, 'show']);
 
 Route::get('authors/{author:username}', function(User $author){
     return view('posts', [
         'posts' => $author->posts,
-        'categories' => Category::all()
     ]);
 });
 
