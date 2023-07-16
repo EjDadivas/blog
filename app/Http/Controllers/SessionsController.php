3756932a483734e290c71bbf1cdfa3755e7ea23c
ashcbrd <99721216+EjDadivas@users.noreply.github.com>
2023-07-16 13:50:58 +0800

    added Comment component

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,13 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create(){
+        return view('sessions.create');
+    }
     public function destroy(){
         // ddd('log the user out');
         auth()->logout();
 
         return redirect('/')->with('success', 'Goodbye!');
     }
+
+    public function store(){
+        // validate request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // attempt and autheniticate user
+        if(!auth()->attempt($attributes)){
+            // if validation fails
+            // return back()
+            //     ->withInput()
+            //     ->withError(['email'=> 'Your provided credentails could not be verified.']);
+
+
+            throw ValidationException::withMessages(
+                ['email'=> 'Your provided credentails could not be verified.',
+                'password' => 'Your password is incorrect'
+            ]);
+
+
+        }
+
+
+
+        // prevent session fixation
+        session()->regenerate();
+         // redirect with flush
+        return redirect('/')->with('success', 'Welcome back');
+
+        }
 }
---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border-gray-200 space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" height="60" width="60" alt="" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted <time>8 months ago</time></p>
+        </header>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Sit ratione dolor ab nemo deserunt, natus hic? Veniam obcaecati voluptatibus consectetur,
+            facilis libero assumenda a sit hic suscipit qui blanditiis dolore beatae nihil, quis perferendis! Dolor commodi quo inventore,
+            libero repellat corrupti necessitatibus eligendi nisi modi voluptas maxime minus! Nesciunt, consequatur.</p>
+    </div>
+
+</article>
